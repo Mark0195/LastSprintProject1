5d440f662b481a944a60f9f22693731b6dd9dd0f
Mark0195 <mark.myles@keyin.com>
2021-12-15 13:30:55 -0330

    added search and password tests

--- src/test/java/DataRestTest/DatabaseTest.java
+++ src/test/java/DataRestTest/DatabaseTest.java
@@ -1,6 +1,8 @@
 package DataRestTest;
 
 import AnimalDatabase.DataRest.mock_data;
+import AnimalDatabase.DataRest.searchtable;
+import AnimalDatabase.DataRest.passwordtable;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,7 +21,7 @@ public class DatabaseTest {
     }
 
     @Test
-    void testAnimalsNotEqual(){
+    void testAnimalsNotEqual() {
         mock_data actualAnimals = new mock_data();
         actualAnimals.setAnimals("Tockus erythrorhyncus");
         actualAnimals.setCommon_name("Red-billed hornbill");
@@ -28,5 +30,27 @@ public class DatabaseTest {
         assertNotEquals("Common zorro", actualAnimals.getCommon_name());
         assertNotEquals(6, actualAnimals.getId());
     }
+
+    @Test
+    void testSearchConstructor() {
+        searchtable actualSearchtable = new searchtable();
+        actualSearchtable.setEmail("jane.doe@example.org");
+        actualSearchtable.setId(123L);
+        actualSearchtable.setSearched("Searched");
+        assertEquals("jane.doe@example.org", actualSearchtable.getEmail());
+        assertEquals(123L, actualSearchtable.getId());
+        assertEquals("Searched", actualSearchtable.getSearched());
+    }
+
+        @Test
+        void testPassConstructor() {
+            passwordtable actualPasswordtable = new passwordtable();
+            actualPasswordtable.setEmail("jane.doe@example.org");
+            actualPasswordtable.setId(123L);
+            actualPasswordtable.setPassword("iloveyou");
+            assertEquals("jane.doe@example.org", actualPasswordtable.getEmail());
+            assertEquals(123L, actualPasswordtable.getId());
+            assertEquals("iloveyou", actualPasswordtable.getPassword());
+
+    }
 }
-//Test123
