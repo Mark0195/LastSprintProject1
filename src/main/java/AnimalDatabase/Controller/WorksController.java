47682a79de3b0022c36d20c030ea418df1e28dd4
alexb454 <71828880+alexb454@users.noreply.github.com>
2021-12-15 14:17:17 -0330

    contoller added for html and configurations started

--- src/main/java/AnimalDatabase/Controller/WorksController.java
+++ src/main/java/AnimalDatabase/Controller/WorksController.java
@@ -1,4 +1,18 @@
 package AnimalDatabase.Controller;
 
-public class WorksController {
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+//@CrossOrigin(origins = "http://localhost:8087")
+//@RestController
+//@RequestMapping("/api")
+//public class WorksController {
+//    @Autowired
+//
+//}
